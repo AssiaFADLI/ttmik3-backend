1fdd3c753d0ca15c03a35486a280c33410e5df8d
AssiaF <assiafadli.pro@gmail.com>
2019-05-20 01:24:09 +0200

    add base project

---
+++ src/main/java/com/viseo/ttmik/TtmikApplication.java
@@ -0,0 +1,13 @@
+package com.viseo.ttmik;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TtmikApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TtmikApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/viseo/ttmik/TtmikApplicationTests.java
@@ -0,0 +1,13 @@
+package com.viseo.ttmik;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TtmikApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
